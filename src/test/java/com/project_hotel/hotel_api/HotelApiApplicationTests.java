254bada12368cd52fd8ed3d52c3270dd33949f59
Sahan samidu saluwadana <shansamidhu@gmail.com>
2025-09-01 04:52:27 +0530

    2nd push

---
+++ src/test/java/com/project_hotel/hotel_api/HotelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project_hotel.hotel_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
